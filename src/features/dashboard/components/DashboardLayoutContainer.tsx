e32c9a0528414084601011ef7f7990601f3e8ec8
jesuspaz3000 <veyito30000@gmail.com>
2025-08-05 23:31:35 -0500

    refactor: check update

--- src/features/dashboard/components/DashboardLayoutContainer.tsx
+++ src/features/dashboard/components/DashboardLayoutContainer.tsx
@@ -146,7 +146,7 @@ function UserMenu() {
   const handleLogout = () => {
     handleClose();
     console.log('Logout clicked');
-    // TODO: Implementar lógica de logout
+    // TODO: Implementar lógica de logout con fe xd
   };
 
   const handleSettings = () => {
