5e50b4f1f0379bd9555ce7470c1189b7aa913429
jesuspaz3000 <veyito30000@gmail.com>
2025-08-06 00:10:21 -0500

    refactor: new check updates

--- src/features/dashboard/components/DashboardLayoutContainer.tsx
+++ src/features/dashboard/components/DashboardLayoutContainer.tsx
@@ -146,7 +146,7 @@ function UserMenu() {
   const handleLogout = () => {
     handleClose();
     console.log('Logout clicked');
-    // TODO: Implementar lógica de logout test nuevo otro
+    // TODO: Implementar lógica de logout test nuevo
   };
 
   const handleSettings = () => {
